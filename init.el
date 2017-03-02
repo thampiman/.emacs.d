20b8f2ad18ae3bcd0313d9d36510854340736c41
Ajay Thampi <ajay.thampi@gmail.com>
2017-03-02 15:12:27 +0000

    First commit

---
+++ init.el
@@ -0,0 +1,10 @@
+(load-file "~/.emacs.d/emacs-for-python/epy-init.el")
+(load-file "~/.emacs.d/themes/afterglow-theme.el")
+
+(add-to-list 'load-path "~/.emacs.d/neotree")
+  (require 'neotree)
+  (global-set-key [f8] 'neotree-toggle)
+
+(setq-default indent-tabs-mode nil)
+(setq tab-width 4) ; or any other preferred value
+(defvaralias 'c-basic-offset 'tab-width)
